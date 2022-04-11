03e27b6dfd81ea30313f4ea86f5c4fa4ed25ca2f
Sebastian <soquendo@email.neit.edu>
2022-04-11 05:48:17 -0400

    start

---
+++ Lan1_oquendo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lan1_oquendo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Int32 intNum1, intNum2, intNum3;
+
+            Double dblNum1 = 0, dblNum2 = 0, dblNum3;
+
+            dblNum3 = dblNum2 * dblNum1
+
+        }
+    }
+}
