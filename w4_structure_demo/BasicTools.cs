df6584d0173a5c8196b77ba177d03b4266d45cc8
Oquendo <soquendo@email.neit.edu>
2022-04-19 10:45:26 -0400

    demo

---
+++ w4_structure_demo/BasicTools.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w4_structure_demo
+{
+    public class BasicTools
+    {
+        /*in the hallway*/
+        public static void credits()
+        {
+            Console.WriteLine("Created by Sebastian Oquendo");
+            Console.WriteLine("Produced by by Sebastian Oquendo");
+            Console.WriteLine("Published by Sebastian Oquendo");
+
+            Console.WriteLine("\n\nPress any key to continue..");
+            Console.ReadKey();
+
+        }
+
+        public static void pause()
+        {
+            Console.WriteLine("\n\nPress any key to continue..");
+            Console.ReadKey();
+
+        }
+
+        public static void CreateBuffer(int lines)
+        {
+            for (int i = 1; i <= lines; i++)
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ w4_structure_demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w4_structure_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BasicTools.credits();
+
+            BasicTools.CreateBuffer(3);
+
+            Console.WriteLine(" 7 + 6 = " + (7 + 6));
+
+            BasicTools.pause();
+
+        }
+    }
+}
