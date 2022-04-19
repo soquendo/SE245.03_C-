3fac898d41a8efdd6d14c6e7304e66d3325baf1c
Oquendo <soquendo@email.neit.edu>
2022-04-19 11:05:19 -0400

    w4

--- w4_structure_demo/Program.cs
+++ w4_structure_demo/Program.cs
@@ -8,6 +8,16 @@ namespace w4_structure_demo
 {
     class Program
     {
+        struct Book
+        {
+            public String Title;
+            public String AuthorFirst;
+            public String AuthorLast;
+            public Double Price;
+            public Int32 Pages;
+            public DateTime PublishDate;
+        }
+
         static void Main(string[] args)
         {
 
@@ -19,6 +29,28 @@ namespace w4_structure_demo
 
             BasicTools.pause();
 
+            Book temp = new Book();
+            //fill in the structure instance
+            temp.Title "Star Wars"
+            temp.AuthorFirst = "Scott"
+            temp.AuthorLast = "Lambert"
+
+            //hacker code runs
+            temp.AuthorFirst += "-PoopyFace";
+            //Print out structure instance info
+            Console.WriteLine(temp.Title + " was directed by " + temp.AuthorFirst + " " + temp.AuthorLast);
+
+            BasicTools.CreateBuffer(3);
+
+            Book temp2 = new Book();
+            //fill in the structure instance
+            temp2.Title "Star Trek"
+            temp2.AuthorFirst = "Scott"
+            temp2.AuthorLast = "Lambert III"
+            //Print out structure instance info
+            Console.WriteLine(temp2.Title + " was directed by " + temp2.AuthorFirst + " " + temp2.AuthorLast);
+
+            BasicTools.pause();
         }
     }
 }
