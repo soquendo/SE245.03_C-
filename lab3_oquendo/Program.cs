1f37ecfa92a99938888bb058cea96ed686e45066
Sebastian <soquendo@email.neit.edu>
2022-04-20 06:48:29 -0400

    3

--- lab3_oquendo/Program.cs
+++ lab3_oquendo/Program.cs
@@ -4,21 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab2_Oquendo
+namespace lab3_oquendo
 {
     class Program
     {
-
         static void Pause()
         {
             Console.WriteLine("\n\nPress any key to end the program..");
-            Console.ReadKey(); 
+            Console.ReadKey();
         }
-
 
         static void Main(string[] args)
         {
-
             //For each student, enter a name and 5 lab grades.  Display each student with each average
             //grade for their 5 labs, and their letter grade.  Drop down a line or two, and then display
             //the average for each lab #.  (Average grade on Lab #1 for all students)
@@ -69,7 +66,7 @@ namespace Lab2_Oquendo
                 strLab1 = Console.ReadLine();
                 intLab1 = Convert.ToInt32(strLab1);
                 lablist1.Add(intLab1);
-                
+
                 Console.Write("Enter your grade for Lab #2 [0-100]: ");
                 strLab2 = Console.ReadLine();
                 intLab2 = Convert.ToInt32(strLab2);
@@ -90,24 +87,24 @@ namespace Lab2_Oquendo
                 intLab5 = Convert.ToInt32(strLab5);
                 lablist5.Add(intLab5);
 
-                labAvg = (intLab1 + intLab2 + intLab3 + intLab4 + intLab5)/5;
+                labAvg = (intLab1 + intLab2 + intLab3 + intLab4 + intLab5) / 5;
                 studentsAvg.Add(labAvg);
 
                 if (labAvg >= 90)
                 {
                     letGrade = "A";
                 }
-                
+
                 else if (labAvg >= 80)
                 {
                     letGrade = "B";
                 }
-                
+
                 else if (labAvg >= 70)
                 {
                     letGrade = "C";
                 }
-                
+
                 else if (labAvg >= 60)
                 {
                     letGrade = "D";
@@ -119,31 +116,41 @@ namespace Lab2_Oquendo
                 }
 
                 letter.Add(letGrade);
-                
+
             }
 
-            double lab1avg = (lablist1[0] + lablist1[1] + lablist1[2] + lablist1[3] + lablist1[4])/5;
-            double lab2avg = (lablist2[0] + lablist2[1] + lablist2[2] + lablist2[3] + lablist2[4])/5;
-            double lab3avg = (lablist3[0] + lablist3[1] + lablist3[2] + lablist3[3] + lablist3[4])/5;
-            double lab4avg = (lablist4[0] + lablist4[1] + lablist4[2] + lablist4[3] + lablist4[4])/5;
-            double lab5avg = (lablist5[0] + lablist5[1] + lablist5[2] + lablist5[3] + lablist5[4])/5;
-            double classavg = (lab1avg + lab2avg + lab3avg + lab4avg + lab5avg)/5;
 
-            Console.WriteLine("Name\t\t" + "Lab1   " + "Lab2   " + "Lab3   " + "Lab4  " + "Lab5  " + "Grade"));
+            double lab1avg = (lablist1[0] + lablist1[1] + lablist1[2] + lablist1[3] + lablist1[4]) / 5;
+            double lab2avg = (lablist2[0] + lablist2[1] + lablist2[2] + lablist2[3] + lablist2[4]) / 5;
+            double lab3avg = (lablist3[0] + lablist3[1] + lablist3[2] + lablist3[3] + lablist3[4]) / 5;
+            double lab4avg = (lablist4[0] + lablist4[1] + lablist4[2] + lablist4[3] + lablist4[4]) / 5;
+            double lab5avg = (lablist5[0] + lablist5[1] + lablist5[2] + lablist5[3] + lablist5[4]) / 5;
+            double classavg = (lab1avg + lab2avg + lab3avg + lab4avg + lab5avg) / 5;
+
+            Console.WriteLine("Name\t\t" + "Lab1   " + "Lab2   " + "Lab3   " + "Lab4  " + "Lab5  " + "Grade");
 
             for (int i = 0; i < totalStudents; i++)
             {
 
-                
+
                 Console.WriteLine(fnames[i] + " " + lnames[i] + "\t\t" + lablist1[i] + "  " + lablist2[i] + "  " + lablist3[i] + "  " + lablist4[i] + "  " + lablist5[i] + "  " + studentsAvg[i] + "  " + letGrade[i]);
 
-                
+
 
                 Console.WriteLine("Class Average:\t" + lab1avg + "  " + lab2avg + "  " + lab3avg + "  " + lab4avg + "  " + lab5avg + "  " + classavg);
 
             }
 
             Pause();
+
+
+
+
+
+
+
+
+
 
         }
     }
