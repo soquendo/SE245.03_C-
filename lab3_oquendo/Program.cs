1b55fdd3be2295994d3f78708e9bf13525f182ac
Oquendo <soquendo@email.neit.edu>
2022-04-21 10:26:59 -0400

    lab3 done

--- lab3_oquendo/Program.cs
+++ lab3_oquendo/Program.cs
@@ -10,9 +10,46 @@ namespace lab3_oquendo
     {
         static void Pause()
         {
-            Console.WriteLine("\n\nPress any key to end the program..");
+            Console.WriteLine("\n\nPress any key to continue..");
             Console.ReadKey();
         }
+
+        static String calcGrade(Double labAvg, String letGrade, List <String> letter)
+        {
+            if (labAvg >= 90)
+            {
+                letGrade = "A";
+            }
+
+            else if (labAvg >= 80)
+            {
+                letGrade = "B";
+            }
+
+            else if (labAvg >= 70)
+            {
+                letGrade = "C";
+            }
+
+            else if (labAvg >= 60)
+            {
+                letGrade = "D";
+            }
+
+            else
+            {
+                letGrade = "F";
+            }
+
+            return letGrade;
+
+        }
+
+        static void calcClass(ref Double classavg, Double numberGrade1, Double numberGrade2, Double numberGrade3, Double numberGrade4, Double numberGrade5)
+        {
+            classavg = (numberGrade1 + numberGrade2 + numberGrade3 + numberGrade4 + numberGrade5) / 5;
+        }
+            
 
         static void Main(string[] args)
         {
@@ -30,18 +67,20 @@ namespace lab3_oquendo
             //Print number grade average and letter grade for each student
             //End prog
 
-            Int32 intLab1, intLab2, intLab3, intLab4, intLab5, totalStudents = 0;
-            Double grades, labAvg, numberGrade1 = 0, numberGrade2 = 0, numberGrade3 = 0, numberGrade4 = 0, numberGrade5 = 0;
-            String strFname, strLname, strLab1, strLab2, strLab3, strLab4, strLab5, letGrade = "F";
+            Double labAvg, numberGrade1 = 0, numberGrade2 = 0, numberGrade3 = 0, numberGrade4 = 0, numberGrade5 = 0;
+            Double classavg = 0, dbltemp, totalStudents = 0, totalgrade = 0;
+            String strFname, strLname, letGrade = "F";
+            String temp;
+            bool bln;
 
             List<String> fnames = new List<String>();
             List<String> lnames = new List<String>();
             List<String> letter = new List<String>();
-            List<Int32> lablist1 = new List<Int32>();
-            List<Int32> lablist2 = new List<Int32>();
-            List<Int32> lablist3 = new List<Int32>();
-            List<Int32> lablist4 = new List<Int32>();
-            List<Int32> lablist5 = new List<Int32>();
+            List<Double> lablist1 = new List<Double>();
+            List<Double> lablist2 = new List<Double>();
+            List<Double> lablist3 = new List<Double>();
+            List<Double> lablist4 = new List<Double>();
+            List<Double> lablist5 = new List<Double>();
             List<Double> studentsAvg = new List<Double>();
             List<Double> classAvg1 = new List<Double>();
             List<Double> classAvg2 = new List<Double>();
@@ -54,6 +93,8 @@ namespace lab3_oquendo
 
             for (int cntr = 1; cntr <= totalStudents; cntr++)
             {
+                totalgrade = 0;
+
                 Console.Write("Please enter student's first name: ");
                 strFname = Console.ReadLine();
                 fnames.Add(strFname);
@@ -62,65 +103,55 @@ namespace lab3_oquendo
                 strLname = Console.ReadLine();
                 lnames.Add(strLname);
 
-                Console.Write("Enter your grade for Lab #1 [0-100]: ");
-                strLab1 = Console.ReadLine();
-                intLab1 = Convert.ToInt32(strLab1);
-                numberGrade1 += intLab1;
-                lablist1.Add(intLab1);
-                
-                Console.Write("Enter your grade for Lab #2 [0-100]: ");
-                strLab2 = Console.ReadLine();
-                intLab2 = Convert.ToInt32(strLab2);
-                numberGrade2 += intLab2;
-                lablist2.Add(intLab2);
+                for (int x = 0; x < 5; x++)
+                {
+                    do
+                    {
+                        Console.Write($"Enter your grade for Lab #{x} [0-100]: ");
+                        temp = Console.ReadLine();
+                        bln = Double.TryParse(temp, out dbltemp);
+                        if (bln == false)
+                        {
+                            Console.Write("Error - Please enter a valid number from 0-100: ");
+                        }
 
-                Console.Write("Enter your grade for Lab #3 [0-100]: ");
-                strLab3 = Console.ReadLine();
-                intLab3 = Convert.ToInt32(strLab3);
-                numberGrade3 += intLab3;
-                lablist3.Add(intLab3);
+                    } while (bln == false);
 
-                Console.Write("Enter your grade for Lab #4 [0-100]: ");
-                strLab4 = Console.ReadLine();
-                intLab4 = Convert.ToInt32(strLab4);
-                numberGrade4 += intLab4;
-                lablist4.Add(intLab4);
+                    totalgrade += dbltemp;
 
-                Console.Write("Enter your grade for Lab #5 [0-100]: ");
-                strLab5 = Console.ReadLine();
-                intLab5 = Convert.ToInt32(strLab5);
-                numberGrade5 += intLab5;
-                lablist5.Add(intLab5);
+                    switch (x)
+                    {
+                        case 0:
+                            lablist1.Add(dbltemp);
+                            numberGrade1 += dbltemp;
+                            break;
+                        case 1:
+                            lablist2.Add(dbltemp);
+                            numberGrade2 += dbltemp;
+                            break;
+                        case 2:
+                            lablist3.Add(dbltemp);
+                            numberGrade3 += dbltemp;
+                            break;
+                        case 3:
+                            lablist4.Add(dbltemp);
+                            numberGrade4 += dbltemp;
+                            break;
+                        case 4:
+                            lablist5.Add(dbltemp);
+                            numberGrade5 += dbltemp;
+                            break;
 
-                labAvg = (intLab1 + intLab2 + intLab3 + intLab4 + intLab5) / 5;
+                    }
+
+                }
+
+                labAvg = totalgrade / 5;
                 studentsAvg.Add(labAvg);
 
-                if (labAvg >= 90)
-                {
-                    letGrade = "A";
-                }
-
-                else if (labAvg >= 80)
-                {
-                    letGrade = "B";
-                }
-
-                else if (labAvg >= 70)
-                {
-                    letGrade = "C";
-                }
-
-                else if (labAvg >= 60)
-                {
-                    letGrade = "D";
-                }
-
-                else
-                {
-                    letGrade = "F";
-                }
-
+                letGrade =  calcGrade(labAvg, letGrade, letter);
                 letter.Add(letGrade);
+
             }
 
             numberGrade1 /= totalStudents;
@@ -128,20 +159,19 @@ namespace lab3_oquendo
             numberGrade3 /= totalStudents;
             numberGrade4 /= totalStudents;
             numberGrade5 /= totalStudents;
-            
 
-            double classavg = (numberGrade1 + numberGrade2 + numberGrade3 + numberGrade4 + numberGrade5) / 5;
+            calcClass(ref classavg, numberGrade1, numberGrade2, numberGrade3, numberGrade4, numberGrade5);
 
-            Console.WriteLine("Name\t\t\t\t" + "Lab1   " + "Lab2   " + "Lab3   " + "Lab4  " + "Lab5  " + "Grade");
+            Console.WriteLine("Name\t\t\t\t" + "Lab1   " + "Lab2   " + "Lab3   " + "Lab4  " + "Lab5  " + " Grade");
 
             for (int i = 0; i < totalStudents; i++)
             {
 
-                Console.WriteLine(String.Format("{0,12} {1,12} {2,5} {3,5} {4,5} {5,5} {6,5} {7,5}  {8,2}", fnames[i], lnames[i] , lablist1[i], lablist2[i], lablist3[i], lablist4[i], lablist5[i], studentsAvg[i], letter[i]));
+                Console.WriteLine(String.Format("{0,12} {1,12}     {2,5}  {3,5}  {4,5}  {5,5} {6,5}\t   {7:0.0} - {8,2}", fnames[i], lnames[i] , lablist1[i], lablist2[i], lablist3[i], lablist4[i], lablist5[i], studentsAvg[i], letter[i]));
 
             }
 
-            Console.WriteLine(String.Format("Class Average:\t\t\t {0:0.0}  {1:0.0}  {2:0.0}  {3:0.0}  {4:0.0}  {5:0.0}", numberGrade1, numberGrade2, numberGrade3, numberGrade4, numberGrade5, classavg));
+            Console.WriteLine(String.Format("Class Average:\t\t\t{0:0.0}   {1:0.0}   {2:0.0}   {3:0.0}  {4:0.0}    {5:0.0}", numberGrade1, numberGrade2, numberGrade3, numberGrade4, numberGrade5, classavg));
 
             Pause();
 
