4dd2e2d75955523f9fc80330c57b4a2f3be593eb
Sebastian <soquendo@email.neit.edu>
2022-04-28 06:57:35 -0400

    4

---
+++ lab4_oquendo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_oquendo
+{
+    class Program
+    {
+        struct person
+        {
+            public string fName;
+            public string mName;
+            public string lName;
+            public string street1;
+            public string street2;
+            public string city;
+            public string state;
+            public string zipCode;
+            public string phone;
+            public string email;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
